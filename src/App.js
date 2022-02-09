febe4e9a2e862557dfa0404322eaa6e9dc0247a7
Gilbert Elnino <gilbeltelnino@gmail.com>
2022-02-09 15:19:01 +0200

    high order components pattern

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
-// import { CurrentUserLoader } from "./CurrentUserLoader";
-import { ResourceLoader } from "./ResourceLoader";
+// import { printProps } from "./printProps";
 import { UserInfo } from "./UserInfo";
-// import { UserLoader } from "./UserLoader";
+import { withUser } from "./withUser";
 import "./App.css";
-import { ProductInfo } from "./ProductInfo";
-import { DataSource } from "./DataSource";
-import axios from "axios";
+import { UserInfoForm } from "./UserInfoForm";
 
-const getServerData = (url) => async () => {
-  const { data } = await axios.get(url);
-  return data;
-};
-
-const getLocalStorageData = (key) => () => {
-  const data = localStorage.getItem(key);
-  return data;
-};
-
-const Text = ({ message }) => <div>{message}</div>;
+// const UserInfoWrappe = printProps(UserInfo);
+const WithUserInfoWrapper = withUser(UserInfo, "1");
 
 function App() {
   return (
-    // <div className="App">
-    //   <UserLoader userId={4}>
-    //     <UserInfo />
-    //   </UserLoader>
-    // </div>
-    // <div className="App">
-    //   <ResourceLoader resourceUrl="/users/1" resourceName="user">
-    //     <UserInfo />
-    //   </ResourceLoader>
-    //   <ResourceLoader resourceUrl="/products/4" resourceName="product">
-    //     <ProductInfo />
-    //   </ResourceLoader>
-    // </div>
     <div className="App">
-      <DataSource getDataFanc={getServerData("/users/1")} resourceName="user">
-        <UserInfo />
-      </DataSource>
-      <DataSource
-        getDataFanc={getLocalStorageData("message")}
-        resourceName="message"
-      >
-        <Text />
-      </DataSource>
+      <h1>HOCs</h1>
+      {/* <UserInfoWrappe a={1} b="yes" /> */}
+      <WithUserInfoWrapper />
+      <UserInfoForm />
     </div>
   );
 }
